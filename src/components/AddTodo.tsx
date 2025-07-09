371bebe882615737ed3403fd7ecd2e609a8a5f76
SeokHwan13 <seokhwan1113@naver.com>
2025-07-09 20:19:56 +0900

    컴포넌트 세팅

---
+++ src/components/AddTodo.tsx
@@ -0,0 +1,10 @@
+function AddTodo() {
+  return (
+    <form>
+      <input placeholder="할 일을 입력하세요" />
+      <button type="submit">추가!</button>
+    </form>
+  );
+}
+
+export default AddTodo;
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,20 @@
+function TodoList() {
+  return (
+    <ul>
+      <li>
+        <input type="checkbox"></input>
+        할일 1<button>삭제</button>
+      </li>
+      <li>
+        <input type="checkbox"></input>
+        할일 2<button>삭제</button>
+      </li>
+      <li>
+        <input type="checkbox"></input>
+        할일 3<button>삭제</button>
+      </li>
+    </ul>
+  );
+}
+
+export default TodoList;
